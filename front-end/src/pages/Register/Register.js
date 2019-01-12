418cee255ef878f97548a4e820be3413bc55e6a8
Deepak Chand <deepu996@gmail.com>
2019-01-12 19:20:08 +0530

    register

---
+++ front-end/src/pages/Register/Register.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from "../../components/Navbar";
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <h1>Register</h1>
+            </div>
+        )
+    }
+}
+
+export default Register;
